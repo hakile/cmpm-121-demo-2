1077d73c7ff927fdb49d5232c97ed6789640366c
Harrison Le <hakile@ucsc.edu>
2023-10-18 03:27:09 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -13,8 +13,56 @@ app.append(header);
 const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
+canvas.style.border = "6px solid #000000";
 canvas.style.borderRadius = "16px";
 canvas.style.boxShadow = "5px 5px 10px rgba(0, 0, 0, .5)";
 app.append(canvas);
 
-console.log("Step 1");
+const ctx = canvas.getContext("2d")!;
+const origin = 0;
+clearCanvas();
+
+const bounds = canvas.getBoundingClientRect();
+const offset = 10;
+
+let lastX = 0;
+let lastY = 0;
+
+let mouseDown = false;
+canvas.onmousedown = function (mouse) {
+  mouseDown = true;
+  lastX = mouse.clientX - bounds.left - offset;
+  lastY = mouse.clientY - bounds.top + offset;
+};
+canvas.onmouseup = function () {
+  mouseDown = false;
+};
+canvas.addEventListener("mousemove", (mouse) => {
+  if (mouseDown) {
+    console.log("eh");
+    const curX: number = mouse.clientX - bounds.left - offset;
+    const curY: number = mouse.clientY - bounds.top + offset;
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(curX, curY);
+    ctx.stroke();
+    lastX = curX;
+    lastY = curY;
+  }
+});
+
+app.append(document.createElement("div"));
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "Clear";
+clearButton.addEventListener("click", clearCanvas);
+app.append(clearButton);
+
+function clearCanvas() {
+  ctx.clearRect(origin, origin, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.fillStyle = "white";
+  ctx.fillRect(origin, origin, canvas.width, canvas.height);
+  ctx.fillStyle = "black";
+}
+
+console.log("Step 2");
