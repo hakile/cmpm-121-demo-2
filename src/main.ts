ecc9c310161733268bd2ca2cdb268d0f9e5bdb43
Harrison Le <hakile@ucsc.edu>
2023-10-18 02:39:13 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,12 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+canvas.style.borderRadius = "16px";
+canvas.style.boxShadow = "5px 5px 10px rgba(0, 0, 0, .5)";
+app.append(canvas);
+
+console.log("Step 1");
