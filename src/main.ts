cb4f350e355723a5f3a9882ef606c205532097c2
Harrison Le <hakile@ucsc.edu>
2023-10-19 22:16:35 -0700

    Step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,37 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "Harrison's Sketchpad";
-const event = new Event("drawing-changed");
+const drawEvent = new Event("drawing-changed");
+
+const undoButton = makeButton("Undo", () => {
+  if (lines.length > 1) {
+    lines.pop();
+    const line = lines.pop();
+    lines.push([]);
+    redos.push(line!);
+    curInd--;
+    canvas.dispatchEvent(drawEvent);
+    redoButton.disabled = false;
+    if (lines.length < 2) undoButton.disabled = true;
+  }
+});
+undoButton.disabled = true;
+
+const clearButton = makeButton("Clear", () => clearCanvas(true));
+
+const redoButton = makeButton("Redo", () => {
+  if (redos.length > 0) {
+    lines.pop();
+    const line = redos.pop();
+    lines.push(line!);
+    lines.push([]);
+    curInd++;
+    canvas.dispatchEvent(drawEvent);
+    undoButton.disabled = false;
+    if (redos.length < 1) redoButton.disabled = true;
+  }
+});
+redoButton.disabled = true;
 
 document.title = gameName;
 
@@ -15,7 +45,6 @@ const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
 canvas.style.border = "6px solid #000000";
-canvas.style.border = "6px solid #000000";
 canvas.style.borderRadius = "16px";
 canvas.style.boxShadow = "5px 5px 10px rgba(0, 0, 0, .5)";
 app.append(canvas);
@@ -23,24 +52,28 @@ app.append(canvas);
 const ctx = canvas.getContext("2d")!;
 const origin = 0;
 
-let points: number[][][] = [[[]]];
+let lines: number[][][] = [[]];
+let redos: number[][][] = [];
 let curInd = 0;
-
-clearCanvas();
 
 let mouseDown = false;
 canvas.onmousedown = function () {
   mouseDown = true;
+  undoButton.disabled = true;
 };
 canvas.onmouseup = function () {
   if (mouseDown) {
     mouseDown = false;
     curInd++;
-    points[curInd] = [[]];
+    lines[curInd] = [[]];
+    undoButton.disabled = false;
+    redos = [];
+    redoButton.disabled = true;
   }
 };
 canvas.addEventListener("drawing-changed", () => {
-  for (const line of points) {
+  clearCanvas(false);
+  for (const line of lines) {
     for (let i = 1; i < line.length; i++) {
       ctx.beginPath();
       ctx.moveTo(line[i - 1][0], line[i - 1][1]);
@@ -53,25 +86,37 @@ canvas.addEventListener("drawing-changed", () => {
 canvas.addEventListener("mousemove", (mouse) => {
   if (mouseDown) {
     const newCoords: number[] = [mouse.offsetX, mouse.offsetY];
-    points[curInd].push(newCoords);
-    canvas.dispatchEvent(event);
+    lines[curInd].push(newCoords);
+    canvas.dispatchEvent(drawEvent);
   }
 });
 
 app.append(document.createElement("div"));
 
-const clearButton = document.createElement("button");
-clearButton.innerHTML = "Clear";
-clearButton.addEventListener("click", () => clearCanvas());
+app.append(undoButton);
 app.append(clearButton);
+app.append(redoButton);
 
-function clearCanvas() {
+clearCanvas(true);
+
+function makeButton(name: string, callback: () => void): HTMLButtonElement {
+  const newButton = document.createElement("button");
+  newButton.innerHTML = name;
+  newButton.addEventListener("click", () => callback());
+  return newButton;
+}
+
+function clearCanvas(clearLines: boolean): void {
   ctx.clearRect(origin, origin, canvas.width, canvas.height);
   ctx.fillStyle = "white";
   ctx.fillRect(origin, origin, canvas.width, canvas.height);
   ctx.fillStyle = "black";
-  points = [[[]]];
-  curInd = 0;
+  if (clearLines) {
+    lines = [[]];
+    undoButton.disabled = true;
+    redoButton.disabled = true;
+    curInd = 0;
+  }
 }
 
-console.log("Step 3");
+console.log("Step 4");
